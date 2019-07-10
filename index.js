5c87c10e6b5760367ff618e6ae60a7358455844a
arumikan <arumikan1234@gmail.com>
2019-07-11 01:15:47 +0900

    change

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ app.get('/',(req,res) => {
   res.send('Welcome to Express')
 });
 
-app.listen(3000,() => {
-  console.log('start server port:3000')
+app.listen(5000,() => {
+  console.log('start server port:5000')
 });
