0e1228fe5125a7ff81c774a397ae4478f2ff4d81
arumikan <arumikan1234@gmail.com>
2019-07-11 11:21:20 +0900

    Improve

--- index.js
+++ index.js
@@ -5,14 +5,22 @@ const app = express();
 app.engine('ejs', ejs.renderFile);
 
 const PORT = process.env.PORT || 5000;
+
 app.get('/',(req,res) => {
   res.send('Welcome to Express')
 });
 
+// app.get('pass',(req,res) => {
+//  <処理（別jsファイルでの処理を挟むか、ここでrenderする変数の処理をする）>
+//  res.render('ejsファイル',{
+//  仮変数: 中身
+//})
+//})
+
 app.get('/test', (req, res) => {
-  res.render('test.ejs',
-    { title: 'Test',
-      content: 'This page is Test page!!'
+  res.render('test.ejs', {
+    title: 'Test',
+    content: 'This page is Test page!!'
   });
 })
 
