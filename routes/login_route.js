b902d2ceef8c1fef1c948f04f475ec3a30db3d31
arumikan <arumikan1234@gmail.com>
2019-07-12 13:49:56 +0900

    Improve

--- routes/login_route.js
+++ routes/login_route.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/login', (req, res) => {
+router.get('/', (req, res) => {
     message = req.session.message;
     req.session.message = '';
     return res.render('login.ejs', {
