b4d6da77bd722ab04a2c556843ebda4c09fa0be6
arumikan <arumikan1234@gmail.com>
2019-07-12 17:56:13 +0900

    Improve

---
+++ public/javascripts/login.js
@@ -0,0 +1,10 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        todos: [
+            { text: 'Learn JavaScript', seen: true },
+            { text: 'Learn Vue', seen: true },
+            { text: 'Build something awesome', seen: false }
+        ]
+    }
+})
