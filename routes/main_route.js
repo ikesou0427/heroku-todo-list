4b79198c2c4ba3736917b1d5ab90316fc47e429f
arumikan <arumikan1234@gmail.com>
2019-07-23 11:34:23 +0900

    Improve

--- routes/main_route.js
+++ routes/main_route.js
@@ -28,7 +28,6 @@ router.get('/', (req, res) => {
     pool.connect((err, client, done) => {
         client.query(sql)
             .then(result => {
-                console.log(result);
                 let m = [], w = [], e = [];
                 for (let i = 0; i < result.rowCount; i++){
                     result.rows[i].attribute == 'm'
